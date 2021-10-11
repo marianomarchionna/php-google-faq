96571492f72bf3ab0b524718ccfa5d60e8560ace
Mariano <marianomarchionna8@gmail.com>
2021-10-11 17:56:23 +0200

    Footer (Fine esercizio)

--- index.php
+++ index.php
@@ -7,14 +7,13 @@
             Per presentare una richiesta di rimozione, compila questo <span class='blue'>modulo web</span>. Riceverai una risposta automatica che conferma la ricezione della tua richiesta. Dopodiché valuteremo il tuo caso (tieni presente che potrebbe occorrere un po' di tempo perché abbiamo già ricevuto tante richieste simili). Nel valutare la richiesta, considereremo se i risultati includono informazioni obsolete relative alla tua vita privata. Considereremo inoltre se vi è un interesse pubblico a che le informazioni rimangano nei nostri risultati delle ricerche, ad esempio se riguardano frodi finanziarie, negligenza professionale, condanne penali o la tua condotta pubblica in relazione a un pubblico ufficio (eletto o non eletto). Queste valutazioni sono complesse e, in quanto organizzazione privata, potremmo non essere nella posizione giusta per prendere decisioni in merito al tuo caso. Se non sei d'accordo con la nostra valutazione, puoi rivolgerti all'Autorità garante per la protezione dei dati personali nel tuo paese.</br></br>
             Nei prossimi mesi lavoreremo a stretto contatto con le autorità per la protezione dei dati e con altre autorità per perfezionare il nostro approccio. La decisione della CGUE rappresenta un cambiamento significativo per i motori di ricerca. Siamo preoccupati per le sue conseguenze, ma riteniamo anche che sia importante rispettare la decisione della Corte e ci stiamo adoperando per predisporre una procedura conforme alla legge.</br></br>
             Quando cerchi un nome, potresti trovare una notifica in cui viene spiegato che i risultati potrebbero essere stati modificati nel rispetto delle leggi europee per la protezione dei dati. Mostriamo questa notifica agli utenti europei quando cercano la maggior parte dei nomi, non soltanto le pagine che sono state soggette a una rimozione.</br></br> "
-
         ],
         [
             'question' => "Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?",
             'answer' => "Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un'elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.</br></br>
             Siamo costantemente al lavoro per garantire un'elevata sicurezza, proteggere la tua privacy e rendere Google ancora più efficace ed efficiente per te. Spendiamo centinaia di milioni di dollari ogni anno per la sicurezza e ci avvaliamo di esperti di fama mondiale in materia di sicurezza dei dati per mantenere le tue informazioni al sicuro. Abbiamo inoltre sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, la verifica in due passaggi e Impostazioni annunci. Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo.</br></br>
             Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel <span class='blue'>Centro Google per la sicurezza online</span>.</br></br>
-            <span class='blue'>Scopri</span> quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo.</br></br>",
+            <span class='blue'>Scopri</span> quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo.</br></br>"
         ],
         [
             'question' => "Perché il mio account è associato a un paese?",
@@ -36,7 +35,7 @@
         ],
         [
             'question' => "Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?",
-            'answer' => "In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di <span class='blue'>URL referrer</span>. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili <span class='blue'>qui</span>. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio.</br></br>",
+            'answer' => "In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di <span class='blue'>URL referrer</span>. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili <span class='blue'>qui</span>. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio.</br></br>"
         ]
     ];
 
@@ -44,27 +43,46 @@
         [
             'title' => 'Introduzione',
             'url' => '#',
-            'status'=>'',
+            'status'=>''
         ],
         [
             'title' => 'Norme sulla privacy',
             'url' => '#',
-            'status'=>'',
+            'status'=>''
         ],
         [
             'title' => 'Termini di servizio',
             'url' => '#',
-            'status'=>'',
+            'status'=>''
         ],
         [
             'title' => 'Tecnologie',
             'url' => '#',
-            'status'=>'',
+            'status'=>''
         ],
         [
             'title' => 'Domande frequenti',
             'url' => '#',
-            'status'=>'active',
+            'status'=>'active'
+        ]
+    ];
+
+    $footerLinks = [
+        [
+            'title' => 'Google',
+            'url' => '#',
+        ],
+        [
+            'title' => 'Tutto su Google',
+            'url' => '#',
+        ],
+        [
+            'title' => 'Privacy',
+            'url' => '#',
+        ],
+        [
+            'title' => 'Termini',
+            'url' => '#',
         ]
     ];
 ?>
@@ -117,5 +135,27 @@
         </div>
     </main> 
     <!-- fine main -->
+    <!-- footer -->
+    <footer>
+        <div class="container">
+            <ul>
+                <?php
+                    foreach($footerLinks as $link){
+                        echo "<li><a href=" . $link['url'] . ">" . $link['title'] . "</a></li>";
+                    }
+                ?>
+            </ul>
+            <form>
+                <i class="fas fa-comment-alt"></i>
+                <select>
+                    <option value="Italiano">Italiano</option>
+                    <option value="English">English</option>
+                    <option value="Espanol">Espanol</option>
+                    <option value="Francais">Francais</option>
+                </select>
+            </form>
+        </div>
+    </footer>
+    <!-- fine footer -->
 </body>
 </html>
